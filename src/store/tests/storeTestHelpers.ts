fdf430ab6678d5e5c97cc53f5b95a0407e7b2e94
Daniel Le Pelley <daniellepelley@gmail.com>
2021-03-08 09:16:03 +0000

    Removed test left in helper do to copy and paste

--- src/store/tests/storeTestHelpers.ts
+++ src/store/tests/storeTestHelpers.ts
@@ -1,18 +1,6 @@
 import { IState } from '../types/IState';
 import reducer from '../reducer';
-import init from '../reducer/generator';
-import { addProduct } from "../actionCreators"
-import staticNames from "../staticNames";
 import Action from '../actions';
-
-test('add 1 product to basket, no discounts', () => {
-  const state: IState = init();
-  const action = addProduct(staticNames.FACE_MASK, 1);
-
-  const newState = reducer(state, action);
-  
-  expect(newState.basket.discounts.length).toEqual(0);
-});
 
 const recApplyActions = (state: IState, actions: Action[]) : IState => {
   const nextAction = actions.shift();
